e866607d4caf5062cfa91aa3960eea04f9d3fbcf
Thiago Pontelli <thiago.pontelli@gmail.com>
2022-12-12 19:16:30 -0500

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+/*  Example 1
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+*/
+
+/* Example 2
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+*/
+
+
