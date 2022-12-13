0a01ad05d0c2edf22918ffb41986d321f485d921
Thiago Pontelli <thiago.pontelli@gmail.com>
2022-12-12 19:30:10 -0500

    More examples

--- conditionals.js
+++ conditionals.js
@@ -32,3 +32,13 @@ if (cold) {
 // if (isCitizen && age > 18) {
 //   console.log("You are eligible to vote.");
 // }
+
+// let temperature = 45
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
+
+let raining = false
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
