d314a12746a964ae6a27028329f257547ae8a646
Thiago Pontelli <thiago.pontelli@gmail.com>
2022-12-15 20:19:43 -0500

    Adding more exercises

--- hello.js
+++ hello.js
@@ -6,3 +6,15 @@ sayHello("Thiago");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// const sayHelloToConsole  = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHelloToConsole('John'); 
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
