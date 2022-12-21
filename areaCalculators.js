5bd3760afc3b3c0c521696331dc410afeb84808b
Thiago Pontelli <thiago.pontelli@gmail.com>
2022-12-20 20:04:18 -0500

    Exercices

---
+++ areaCalculators.js
@@ -0,0 +1,29 @@
+function calculateRectangleArea(length, width) {
+  if (length * width > 0) {
+    return length * width;
+  }
+}
+
+function calculateTriangleArea(base, height) {
+  if (base * height / 2 > 0) {
+    return base * height / 2;
+  }
+}
+
+function calculateCircleArea(radius) {
+  if (radius > 0) {
+    return Math.PI * radius ** 2;
+  }  
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
